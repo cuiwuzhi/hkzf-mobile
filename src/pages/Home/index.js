3d2f34d1f75a45f037a387ddb7a4a5d620e56e13
cuiwuzhi <cuiwuzhi@tanzhishuju.com>
2019-11-14 20:08:00 +0800

    '首页租房小组模块'

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,68 +1,68 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
-import { TabBar } from 'antd-mobile'
-import './index.css'
+import {Route} from 'react-router-dom'
+import {TabBar} from 'antd-mobile'
+import './index.scss'
 import Index from '../Index'
 import FindHouse from '../FindHouse'
 import News from '../News'
 import Profile from '../Profile'
 
 const tabBarItems = [
-  { title: '首页', icon: 'icon-ind', path: '/home' },
-  { title: '找房', icon: 'icon-findHouse', path: '/home/findHouse' },
-  { title: '资讯', icon: 'icon-infom', path: '/home/news' },
-  { title: '我的', icon: 'icon-my', path: '/home/profile' }
+	{title: '首页', icon: 'icon-ind', path: '/home'},
+	{title: '找房', icon: 'icon-findHouse', path: '/home/findHouse'},
+	{title: '资讯', icon: 'icon-infom', path: '/home/news'},
+	{title: '我的', icon: 'icon-my', path: '/home/profile'}
 ]
 
 export default class Home extends React.Component {
-  state = {
-    selectedTab: this.props.location.pathname
-  }
+	state = {
+		selectedTab: this.props.location.pathname, // 选中的tab
+		contentHeight: document.documentElement.clientHeight || document.body.clientHeight
+	}
 
-  componentDidUpdate(preProps) {
-    // 解决路由切换时tab高亮问题
-    if (preProps.location.pathname !== this.props.location.pathname) {
-      this.setState({
-        selectedTab: this.props.location.pathname
-      })
-    }
-  }
+	componentDidUpdate(preProps) {
+		// 解决路由切换时tab高亮问题
+		if (preProps.location.pathname !== this.props.location.pathname) {
+			this.setState({
+				selectedTab: this.props.location.pathname
+			})
+		}
+	}
 
-  renderTabItem() {
-    return tabBarItems.map(item => <TabBar.Item
-      title={item.title}
-      key={item.title}
-      icon={<i className={`iconfont ${item.icon}`} />}
-      selectedIcon={<i className={`iconfont ${item.icon}`} />}
-      selected={this.state.selectedTab === item.path}
-      onPress={() => {
-        this.setState({
-          selectedTab: item.path
-        })
-        this.props.history.push(item.path)
-      }}
-    ></TabBar.Item>
-    )
-  }
+	renderTabItem() {
+		return tabBarItems.map(item => <TabBar.Item
+			title={item.title}
+			key={item.title}
+			icon={<i className={`iconfont ${item.icon}`}/>}
+			selectedIcon={<i className={`iconfont ${item.icon}`}/>}
+			selected={this.state.selectedTab === item.path}
+			onPress={() => {
+				this.setState({
+					selectedTab: item.path
+				})
+				this.props.history.push(item.path)
+			}}
+		  ></TabBar.Item>
+		)
+	}
 
-  render() {
-    return (
-      <div className="home">
-        <Route exact path="/home" component={Index} />
-        <Route path="/home/findHouse" component={FindHouse} />
-        <Route path="/home/news" component={News} />
-        <Route path="/home/profile" component={Profile} />
-
-        {/* <div className="tabBar-wrap"> */}
-        <TabBar
-          tintColor="#21b97a"
-          barTintColor="white"
-          noRenderContent={true}
-        >
-          {this.renderTabItem()}
-        </TabBar>
-        {/* </div> */}
-      </div>
-    )
-  }
+	render() {
+		return (
+		  <div className="home">
+			  <div className="content" style={{height: this.state.contentHeight - 50}}>
+				  <Route exact path="/home" component={Index}/>
+				  <Route path="/home/findHouse" component={FindHouse}/>
+				  <Route path="/home/news" component={News}/>
+				  <Route path="/home/profile" component={Profile}/>
+			  </div>
+			  <TabBar
+				tintColor="#21b97a"
+				barTintColor="white"
+				noRenderContent={true}
+			  >
+				  {this.renderTabItem()}
+			  </TabBar>
+		  </div>
+		)
+	}
 }
--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Carousel, Flex } from 'antd-mobile'
+import {Carousel, Flex, Grid} from 'antd-mobile'
 import './index.scss'
 import axios from 'axios'
 // 导航栏图片
@@ -8,92 +8,104 @@ import Nav2 from '../../assets/images/nav-2.png'
 import Nav3 from '../../assets/images/nav-3.png'
 import Nav4 from '../../assets/images/nav-4.png'
 
+// 导航菜单数据
 const navs = [
-  { id: 1, img: Nav1, title: '整租', path: '/home/findHouse' },
-  { id: 2, img: Nav2, title: '合租', path: '/home/findHouse' },
-  { id: 3, img: Nav3, title: '地图找房', path: '/map' },
-  { id: 4, img: Nav4, title: '出租', path: '/home/findHouse' }
+	{id: 1, img: Nav1, title: '整租', path: '/home/findHouse'},
+	{id: 2, img: Nav2, title: '合租', path: '/home/findHouse'},
+	{id: 3, img: Nav3, title: '地图找房', path: '/map'},
+	{id: 4, img: Nav4, title: '出租', path: '/home/findHouse'}
 ]
 
+const data = Array.from(new Array(9)).map((_val, i) => ({
+	icon: 'https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png',
+	text: `name${i}`,
+}));
+
 export default class Index extends React.Component {
-  state = {
-    swipers: [], // 轮播图数据
-    isSwiperLoaded: false, // 轮播图数据是否加载完成
-    groups: [] // 租房小组数据
-  }
+	state = {
+		swipers: [], // 轮播图数据
+		isSwiperLoaded: false, // 轮播图数据是否加载完成
+		groups: [] // 租房小组数据
+	}
 
-  componentDidMount() {
-    this.getSwipers()
-    this.getGroups()
-  }
+	componentDidMount() {
+		this.getSwipers()
+		this.getGroups()
+	}
 
-  // 获取轮播图
-  async getSwipers() {
-    const { data: res } = await axios.get('http://localhost:8080/home/swiper')
-    this.setState({
-      swipers: res.body,
-      isSwiperLoaded: true
-    })
-  }
+	// 获取轮播图
+	async getSwipers() {
+		const {data: res} = await axios.get('http://192.168.199.181:8080/home/swiper')
+		this.setState({
+			swipers: res.body,
+			isSwiperLoaded: true
+		})
+	}
 
-  // 获取租房小组的数据
-  async getGroups() {
-    const {data: res} = await axios.get('http://localhost:8080/home/groups', {
-      params: {
-        area: 'AREA|88cff55c-aaa4-e2e0'
-      }
-    })
-    this.setState({
-      groups: res.body
-    })
-  }
+	// 获取租房小组的数据
+	async getGroups() {
+		const {data: res} = await axios.get('http://192.168.199.181:8080/home/groups', {
+			params: {
+				area: 'AREA|88cff55c-aaa4-e2e0'
+			}
+		})
+		this.setState({
+			groups: res.body
+		})
+	}
 
-  // 渲染轮播图结构
-  renderSwipers() {
-    return this.state.swipers.map(item =>
-      <a
-        key={item.id}
-        href="http://www.alipay.com"
-        style={{ display: 'inline-block', width: '100%', height: 212 }}
-      >
-        <img
-          src={`http://192.168.1.100:8080${item.imgSrc}`}
-          alt="这是轮播图"
-          style={{ width: '100%', verticalAlign: 'top' }}
-        />
-      </a>
-    )
-  }
+	// 渲染轮播图结构
+	renderSwipers() {
+		return this.state.swipers.map(item =>
+		  <a
+			key={item.id}
+			href="http://www.alipay.com"
+			style={{display: 'inline-block', width: '100%', height: 212}}
+		  >
+			  <img src={`http://192.168.199.181:8080${item.imgSrc}`} alt="swiper"
+			       style={{width: '100%', verticalAlign: 'top'}}/>
+		  </a>
+		)
+	}
 
-  //  渲染导航栏的Item
-  renderNavs() {
-    return navs.map(item =>
-      <Flex.Item key={item.id} onClick={() => this.props.history.push(item.path)}>
-        <img src={item.img} alt="icon" />
-        <h2>{item.title}</h2>
-      </Flex.Item>
-    )
-  }
+	//  渲染导航栏的Item
+	renderNavs() {
+		return navs.map(item =>
+		  <Flex.Item key={item.id} onClick={() => this.props.history.push(item.path)}>
+			  <img src={item.img} alt="icon"/>
+			  <h2>{item.title}</h2>
+		  </Flex.Item>
+		)
+	}
 
-  render() {
-    return (
-      <div className="index">
-        {/* 轮播图 */}
-        <div className="swiper">
-          {this.state.isSwiperLoaded ? (
-            <Carousel autoplay infinite>
-              {this.renderSwipers()}
-            </Carousel>
-          ) : ('')
-          }
-        </div>
+	render() {
+		return (
+		  <div className="index">
+			  {/* 轮播图 */}
+			  <div className="swiper">
+				  {this.state.isSwiperLoaded ? (
+					<Carousel autoplay infinite>
+						{this.renderSwipers()}
+					</Carousel>
+				  ) : ('')
+				  }
+			  </div>
 
-        {/* 导航 */}
-        <Flex className="nav">
-          {this.renderNavs()}
-        </Flex>
-      </div>
-    )
-  }
+			  {/* 导航 */}
+			  <Flex className="nav">
+				  {this.renderNavs()}
+			  </Flex>
+
+			  {/* 租房小组 */}
+			  <div className="group">
+				  <h3 className="title">
+					  租房小组 <span className="more">更多</span>
+				  </h3>
+				  {/* 宫格组件 */}
+				  <Grid data={data}/>
+			  </div>
+		  </div>
+		)
+	}
 
 }
